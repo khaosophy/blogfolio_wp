cce8c0652692be214a701b4b0fc5a8605bc6528b
Casey James <casey@khaosophy.com>
2020-05-19 11:30:14 -0400

    created custom fields for speaker sessions

--- wp-content/mu-plugins/cpt_code_samples.php
+++ wp-content/mu-plugins/cpt_code_samples.php
@@ -5,20 +5,20 @@ Plugin Name: Casper's Code Samples
 // Add Custom Post Type for Code Samples
 function cjp_code_sample_post_type(){
 	$labels = array(
-        'name'                  => _x( 'Code Samples', 'cjp_blogfolio' ),
-        'singular_name'         => _x( 'Code Sample', 'cjp_blogfolio' ),
+    'name'                  => _x( 'Code Samples', 'cjp_blogfolio' ),
+    'singular_name'         => _x( 'Code Sample', 'cjp_blogfolio' ),
 	);
 	
 	$args = array(
 		'labels'             => $labels,
-        'public'             => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'code-samples' ),
-		'show_in_rest'		 => true,
-        'rest_base'          => 'code-samples',
-        'has_archive'        => true,
-        'menu_position'      => 5,
-        'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+    'public'             => true,
+    'query_var'          => true,
+    'rewrite'            => array( 'slug' => 'code-samples' ),
+		'show_in_rest'	  	 => true,
+    'rest_base'          => 'code-samples',
+    'has_archive'        => true,
+    'menu_position'      => 5,
+    'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
 	);
 	
 	register_post_type( 'cjp_code_sample', $args );
--- wp-content/mu-plugins/cpt_speaker_sessions.php
+++ wp-content/mu-plugins/cpt_speaker_sessions.php
@@ -5,22 +5,84 @@ Plugin Name: Casper's Speaker Sessions
 // Add Custom Post Type for Speaker Sessions 
 function cjp_speaker_session_post_type(){
 	$labels = array(
-        'name'                  => _x( 'Speaker Sessions', 'twentyseventeen-child' ),
-        'singular_name'         => _x( 'Speaker Session', 'twentyseventeen-child' ),
+    'name'                  => _x( 'Speaker Sessions', 'cjp_blogfolio' ),
+    'singular_name'         => _x( 'Speaker Session', 'cjp_blogfolio' ),
 	);
 	
 	$args = array(
-		'labels'             => $labels,
-        'public'             => true,
-        'query_var'          => true,
-        'rewrite'            => array( 'slug' => 'speaker-sessions' ),
-        'rest_base'          => 'speaker-sessions',
-		'show_in_rest'		 => true,
-        'has_archive'        => true,
-        'menu_position'      => 5,
-        'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+    'labels'             => $labels,
+    'public'             => true,
+    'query_var'          => true,
+    'rewrite'            => array( 'slug' => 'speaker-sessions' ),
+    'rest_base'          => 'speaker-sessions',
+    'show_in_rest'	  	 => true,
+    'has_archive'        => true,
+    'menu_position'      => 5,
+    'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
 	);
 	
 	register_post_type( 'cjp_speaker_session', $args );
 }
 add_action('init', 'cjp_speaker_session_post_type');
+
+
+// Add Advanced Custom Fields
+function cjp_speaker_session_custom_fields() {  
+  acf_add_local_field_group(array (
+    'key'       => 'cjp_speaker_session_meta_1',
+    'title'     => 'Meta Info',
+    'position'  => 'side',
+    'location'  => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'cjp_speaker_session',
+        )
+      )
+    ),
+    'fields' => array (
+      array (
+        // date of talk
+        'key'     => 'cjp_talk_date',
+        'label'   => 'Date of Talk',
+        'name'    => 'talk_date',
+        'type'    => 'date_picker',
+        'display_format' => 'm/d/yy',
+        'return_format' => 'm/d/yy',
+        'first_day' => '0',
+        'required' => 'true',
+      ),
+      array (
+        // start time of talk
+        'key' => 'cjp_talk_start',
+        'label' => 'Start Time',
+        'name' => 'talk_start',
+        'type' => 'time_picker',
+      ),
+      array (
+        // end time of talk
+        'key' => 'cjp_talk_end',
+        'label' => 'End Time',
+        'name' => 'talk_end',
+        'type' => 'time_picker',
+      ),
+      array (
+        // where to register
+        'key' => 'cjp_talk_registration',
+        'label' => 'Registration URL',
+        'name' => 'talk_registration',
+        'type' => 'text',
+      ),
+      array (
+        // link to recording
+        'key' => 'cjp_talk_recording',
+        'label' => 'Recording URL',
+        'name' => 'talk_recording',
+        'type' => 'text',
+      ),
+    )
+  ));
+}
+
+add_action('acf/init', 'cjp_speaker_session_custom_fields');
